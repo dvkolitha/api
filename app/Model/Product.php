fc1d079aa4831e87b6f9615e66c1ec46b984841d
Kolitha <dvkolitha@gmail.com>
2018-10-08 23:27:46 +0530

    Product Resource was created

--- app/Model/Product.php
+++ app/Model/Product.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    publick function reviews()
+    public function reviews()
     {
     	return $this->hasMany(Product::class);
     }
